becff2ba7d2da6dc799ec278047fb38be5b542e6
Mahmoud Hamsho <36112117+hamshom@users.noreply.github.com>
2019-05-09 03:13:49 -0700

    Add files via upload
    update

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, json, request, Response, jsonify, flas
 import queries as query
 from flask_mysqldb import MySQL
 import database as db
-from random import randint
 import requests
+
 
 
 app = Flask(__name__)
@@ -43,8 +43,76 @@ def google():
 
     zipCode = str(request.form['zipCode'])
     data = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address={zipCode}&key=AIzaSyD5GiLT_Ny1Lrbad3IrMujyaTWaqMbllr4').json()
-
+    print('-----------------------')
+    print('City Geographical Features:')
     print(json.dumps(data, indent=2))
+    # dataJson = json.dumps(data)
+    # state = data['results'][0]['address_components'][3]['long_name']
+    city_name  = data['results'][0]['address_components'][1]['long_name']
+
+    # Enter your API key here
+    api_key = "c6c29f0756c11924fedd6b244702cca4"
+
+    # base_url variable to store url
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    # complete_url variable to store
+    # complete url address
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+    # get method of requests module
+    # return response object
+    response = requests.get(complete_url)
+
+    # json method of response object
+    # convert json format data into
+    # python format data
+    x = response.json()
+    # Now x contains list of nested dictionaries
+    # Check the value of "cod" key is equal to
+    # "404", means city is found otherwise,
+    # city is not found
+    if x["cod"] != "404":
+
+        # store the value of "main"
+        # key in variable y
+        y = x["main"]
+
+        # store the value corresponding
+        # to the "temp" key of y
+        current_temperature = y["temp"]
+
+        # store the value corresponding
+        # to the "pressure" key of y
+        current_pressure = y["pressure"]
+
+        # store the value corresponding
+        # to the "humidity" key of y
+        current_humidiy = y["humidity"]
+
+        # store the value of "weather"
+        # key in variable z
+        z = x["weather"]
+
+        # store the value corresponding
+        # to the "description" key at
+        # the 0th index of z
+        weather_description = z[0]["description"]
+
+        # print following values
+        print(f'\n{city_name} weather information:')
+        print(" Temperature (in kelvin unit) = " +
+              str(current_temperature) +
+              "\n atmospheric pressure (in hPa unit) = " +
+              str(current_pressure) +
+              "\n humidity (in percentage) = " +
+              str(current_humidiy) +
+              "\n description = " +
+              str(weather_description))
+
+    else:
+        print(" City Not Found ")
+
 
     return "Success"
 
